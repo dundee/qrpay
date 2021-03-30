185c7c48e575b8a91ea788173eb3c51857591744
Daniel Milde <daniel@milde.cz>
2021-03-31 00:05:34 +0200

    added usage to main package

--- payment.go
+++ payment.go
@@ -1,3 +1,53 @@
+/*
+Package for creating QR codes for payments.
+
+[Short Payment Descriptor](https://en.wikipedia.org/wiki/Short_Payment_Descriptor) format and
+[EPC QR Code](https://en.wikipedia.org/wiki/EPC_QR_code) (SEPA) format is supported.
+
+## Usage
+
+### Generating QR code image for Short Payment Descriptor format
+
+	import payment "github.com/dundee/go-qrcode-payment"
+
+	p := payment.NewSpaydPayment()
+	p.SetIBAN("CZ5855000000001265098001")
+	p.SetAmount("10.8")
+	p.SetDate(time.Date(2021, 12, 24, 0, 0, 0, 0, time.UTC))
+	p.SetMessage("M")
+	p.SetRecipientName("go")
+	p.SetNofificationType('E')
+	p.SetNotificationValue("daniel@milde.cz")
+	p.SetExtendedAttribute("vs", "1234567890")
+
+	payment.SaveQRCodeImageToFile(p, "qr-payment.png")
+
+### Generating QR code image for EPC QR Code
+
+	import payment "github.com/dundee/go-qrcode-payment"
+
+	p := payment.NewEpcPayment()
+	p.SetIBAN("CZ5855000000001265098001")
+	p.SetAmount("10.8")
+	p.SetMessage("M")
+	p.SetRecipientName("go")
+
+	payment.SaveQRCodeImageToFile(p, "qr-payment.png")
+
+QR code image encoding uses [skip2/go-qrcode](https://github.com/skip2/go-qrcode).
+
+### Getting QR code content for Short Payment Descriptor format
+
+	import payment "github.com/dundee/go-qrcode-payment"
+
+	p := payment.NewSpaydPayment()
+	p.SetIBAN("CZ5855000000001265098001")
+	p.SetAmount("108")
+
+	fmt.Println(payment.GenerateString())
+	// Output: SPD*1.0*ACC:CZ5855000000001265098001*AM:108*
+
+*/
 package payment
 
 import (
